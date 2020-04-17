d9ab9609ab42ab752c22d80e752b0f762f84d515
Luis Fernando Zaltron <lfzaltron@gmail.com>
2020-04-17 18:01:44 -0300

    create appointments service

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,37 @@
+import { startOfHour } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import Appointment from '../models/Appointment';
+
+interface Request {
+    provider: string;
+    date: Date;
+}
+
+class CreateAppointmentService {
+    private appointmentsRepository: AppointmentsRepository;
+
+    constructor(appointmentsRepository: AppointmentsRepository) {
+        this.appointmentsRepository = appointmentsRepository;
+    }
+
+    public execute({ provider, date }: Request): Appointment {
+        const parsedDate = startOfHour(date);
+
+        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+            parsedDate,
+        );
+
+        if (findAppointmentInSameDate) {
+            throw Error('This appointment is already booked.');
+        }
+
+        const appointment = this.appointmentsRepository.create({
+            provider,
+            date: parsedDate,
+        });
+
+        return appointment;
+    }
+}
+
+export default CreateAppointmentService;
