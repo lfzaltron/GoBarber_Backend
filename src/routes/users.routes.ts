f49b0ee6c0a3a9d2b11a0fdfaadc4710e92d90a8
Luis Fernando Zaltron <lfzaltron@gmail.com>
2020-04-18 22:13:58 -0300

    upload images

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -18,5 +23,24 @@ usersRouter.post('/', async (request, response) => {
         return response.status(400).json({ message: error.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        try {
+            const updateUserAvatarService = new UpdateUserAvatarService();
+            const user = await updateUserAvatarService.execute({
+                user_id: request.user.id,
+                avatarFilename: request.file.filename,
+            });
+            delete user.password;
+            return response.json(user);
+        } catch (error) {
+            return response.status(400).json({ message: error.message });
+        }
+    },
+);
 
 export default usersRouter;
